9946978959fc8a2e31827f0b514a7cff40015f93
Q-Znikon <85625583+Q-Znikon@users.noreply.github.com>
2021-09-06 23:28:59 +0700

    Added csv, and exit command.

--- speech.py
+++ speech.py
@@ -1,21 +1,29 @@
+# v.2
 import speech_recognition as sr
 from datetime import datetime
 
-while True:
-    r = sr.Recognizer()
+r = sr.Recognizer()
+mic = sr.Microphone()
 
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source)
+with mic as source:
+	r.adjust_for_ambient_noise(source)
+	audio = r.listen(source)
 
-        time = datetime.now().strftime('%H:%M:%S')
+	while True:
 
-        print(time, "Say something:")
+		response = {
+			"success": True,
+			"error": None,
+			"transcription": None
+		}
 
-        audio = r.listen(source)
 
-        try:
-            words = r.recognize_google(audio, language="th-TH")
-            print(time, words, "\n")
+		try:
+			words = r.recognize_google(audio, language="th-TH")
+		except sr.RequestError:
+			response["success"] = False
+			response["error"] = "API unavailable"
+		except sr.UnknownValueError:
+			response["error"] = "Unable to recognize speech"
 
-        except Exception as e:
-            print(time, "Error", e, "\n")
+		#return response
