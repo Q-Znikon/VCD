9f8b04faccaa5b4aed29857963955bf1887ea3e9
Q-Znikon <85625583+Q-Znikon@users.noreply.github.com>
2021-09-06 23:51:08 +0700

    added csv, and exit command (2)

--- speech.py
+++ speech.py
@@ -1,29 +1,36 @@
-# v.2
+# v.2, added csv and exit command.
 import speech_recognition as sr
 from datetime import datetime
+import csv
 
-r = sr.Recognizer()
-mic = sr.Microphone()
 
-with mic as source:
-	r.adjust_for_ambient_noise(source)
-	audio = r.listen(source)
+device_name = input("Device name: ")
 
-	while True:
+while True:
+	r = sr.Recognizer()
+	mic = sr.Microphone()
 
-		response = {
-			"success": True,
-			"error": None,
-			"transcription": None
-		}
+	with mic as source:
+		r.adjust_for_ambient_noise(source)
 
+		time = datetime.now().strftime('%H:%M:%S')
+		
+		print("\n",time, "Say something:")
+
+		audio = r.listen(source)
 
 		try:
 			words = r.recognize_google(audio, language="th-TH")
-		except sr.RequestError:
-			response["success"] = False
-			response["error"] = "API unavailable"
-		except sr.UnknownValueError:
-			response["error"] = "Unable to recognize speech"
+			print(words)
+			with open('speech_timestamp.csv', 'a', encoding='utf-8') as f:
+				fw = csv.writer(f)
+				data = [time, words, device_name]
+				fw.writerow(data)
 
-		#return response
+			if words == "ออก":
+				print("Goodbye.")
+				break
+
+		except Exception as e:
+			print("Error", e)
+
